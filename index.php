15f9205f61ff3b08c9470d6ad5626fd30e6539bd
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-09-22 16:51:52 -0500

    feat: Add index file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hello!</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+<?php
+include('header.php');
+?>
+
+<body>
+
+    <?php
+    include('footer.php');
+    ?>
